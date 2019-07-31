65848e286ae6b9cd12f378bb71a2972c6ed53be4
david.baena <david.baena@bq.com>
2019-07-31 22:28:31 +0200

    Facets done

--- chapter3/chapter3_core_aggregation.js
+++ chapter3/chapter3_core_aggregation.js
@@ -81,8 +81,141 @@ var pipeline3 = [{
   },
   {
     $sort: {
-      count:-1
+      count: -1
     }
   }
 ]
 db.air_routes.aggregate(pipeline3)
+
+//graphLookup
+{
+  "_id": 6,
+  "name": "Karen",
+  "title": "CTO",
+  "reports_to": 3
+} {
+  "_id": 3,
+  "name": "Eliot",
+  "title": "CTO",
+  "reports_to": 1
+}
+
+{
+  "_id": ObjectId("56e9b497732b6122f8790280"),
+  "airline": 4,
+  "name": "2 Sqn No 1 Elementary Flying Training School",
+  "alias": "",
+  "iata": "WYT",
+  "icao": "",
+  "active": "N",
+  "country": "United Kingdom",
+  "base": "HGH"
+}
+
+{
+  "_id": ObjectId("56e9b39b732b6122f877fa31"),
+  "airline": {
+    "id": 410,
+    "name": "Aerocondor",
+    "alias": "2B",
+    "iata": "ARD"
+  },
+  "src_airport": "CEK",
+  "dst_airport": "KZN",
+  "codeshare": "",
+  "stops": 0,
+  "airplane": "CR2"
+}
+
+{
+  "_id": ObjectId("581288b9f374076da2e36fe5"),
+  "name": "Star Alliance",
+  "airlines": [
+    "Air Canada",
+    "Adria Airways",
+    "Avianca",
+    "Scandinavian Airlines",
+    "All Nippon Airways",
+    "Brussels Airlines",
+    "Shenzhen Airlines",
+    "Air China",
+    "Air New Zealand",
+    "Asiana Airlines",
+    "Brussels Airlines",
+    "Copa Airlines",
+    "Croatia Airlines",
+    "EgyptAir",
+    "TAP Portugal",
+    "United Airlines",
+    "Turkish Airlines",
+    "Swiss International Air Lines",
+    "Lufthansa",
+    "EVA Air",
+    "South African Airways",
+    "Singapore Airlines"
+  ]
+}
+
+
+//facets
+var pipeline4 = [{
+    $match: {
+      "imdb.rating": {
+        $gte: 0
+      },
+      "metacritic": {
+        $gte: 0
+      }
+    }
+  },
+  {
+    $project: {
+      _id: 0,
+      "metacritic": 1,
+      "imdb": 1,
+      "title": 1
+    }
+  },
+  {
+    $facet: {
+      top_metacritic: [{
+          $sort: {
+            "metacritic": -1,
+            "title": 1
+          }
+        },
+        {
+          $limit: 10
+        },
+        {
+          $project: {
+            title: 1
+          }
+        }
+      ],
+      top_imdb: [{
+          $sort: {
+            "imdb.rating": -1,
+            "title": 1
+          }
+        },
+        {
+          $limit: 10
+        },
+        {
+          $project: {
+            "title": 1
+          }
+        }
+      ]
+    }
+  },
+  {
+    $project: {
+      movies_in_both: {
+        $setIntersection: ["$top_metacritic", "top_imdb"]
+      }
+    }
+  }
+]
+db.air_routes.aggregate(pipeline4)
